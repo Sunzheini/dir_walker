d3ddb32222856d4b796616f505cf9d8b77d50819
Sunzheini <daniel_zorov@abv.bg>
2022-12-09 19:58:51 +0200

    update_09122022

--- main.py
+++ main.py
@@ -3,139 +3,125 @@ import time
 from os import walk
 from os.path import exists
 from tkinter import filedialog
-
-import openpyxl
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
-from openpyxl.styles import Font
-
+from openpyxl import Workbook
 from tkinter import *
 
-
-# custom functions
-# --------------------------------------------------------------------
 
 def time_measurement_decorator(some_function):
     def wrapper(*args, **kwargs):
         start = time.time()
-        result = some_function(*args, **kwargs)     # the function
+        result = some_function(*args, **kwargs)
         end = time.time()
         measurement = end - start
-        label3.config(text=f"generated in: {measurement:.5f}")
-        return result                               # the function
+        new_gui.label3.config(text=f"generated in: {measurement:.5f}")
+        return result
     return wrapper
 
 
-def empty_the_output_txt_file(file_path):
-    if exists(file_path):
-        os.remove(file_path)
+class DirWalkerGui:
+    def __init__(self):
+        self.current_folder = ''
+        self.generated_txt_file_path = './generated_txt_file.txt'
+        self.generated_excel_file_path = './generated_excel_file.xlsx'
+        self.current_excel_cell = 'A1'
+
+        self.workbook = None
+        self.worksheet = None
+
+        self.window = Tk()
+        self.window.geometry("300x300")
+        self.window.title("dir walker")
+        self.window.config(background='#2b2828')
+
+        self.label1 = Label(
+            self.window, text='select target dir', width=30, height=1,
+            bg='#2b2828', borderwidth=0, relief="ridge", fg='white'
+        )
+        self.label1.pack()
+
+        self.button1 = Button(
+            self.window, text='browse', width=30, height=1,
+            command=self.open_dir
+        )
+        self.button1.pack()
+
+        self.label2 = Label(
+            self.window, text=self.current_folder, width=30, height=1,
+            bg='#2b2828', borderwidth=0, relief="ridge", fg='white'
+        )
+        self.label2.pack()
+
+        self.button2 = Button(
+            self.window, text='generate', width=30, height=1,
+            command=self.the_walk_loop
+        )
+        self.button2.pack()
+
+        self.label3 = Label(
+            self.window, text='', width=30, height=1,
+            bg='#2b2828', borderwidth=0, relief="ridge", fg='white'
+        )
+        self.label3.pack()
+
+    @staticmethod
+    def empty_the_output_txt_file(file_path):
+        if exists(file_path):
+            os.remove(file_path)
+
+    def empty_the_output_excel_file(self, file_path):
+        if exists(file_path):
+            os.remove(file_path)
+
+        self.workbook = Workbook()
+        self.worksheet = self.workbook.active
+        self.worksheet.title = "Data"
+
+    @staticmethod
+    def write_into_txt_file(file_path, content):
+        with open(file_path, 'a') as output_file:
+            output_file.write(content + '\n')
+
+    def write_into_excel_file(self, file_path, content):
+        self.worksheet.append([content])
+        self.workbook.save(file_path)
+
+    def open_dir(self):
+        filepath = filedialog.askdirectory()
+        self.current_folder = filepath
+        self.label2.config(text=f"selected: '{self.current_folder}'")
+
+    @staticmethod
+    def fix_the_dir_path_str(dir_path):
+        dir_path = dir_path[0:2] + '/' + dir_path[3:]
+        return dir_path
+
+    @time_measurement_decorator
+    def the_walk_loop(self):
+        self.empty_the_output_txt_file(self.generated_txt_file_path)
+        self.empty_the_output_excel_file(self.generated_excel_file_path)
+
+        folder = self.current_folder
+
+        for (dir_path, dir_names, file_names) in walk(folder):
+            dir_path = self.fix_the_dir_path_str(dir_path)
+
+            self.write_into_txt_file(self.generated_txt_file_path, f'Directory: {dir_path}')
+            self.write_into_excel_file(self.generated_excel_file_path, f'Directory: {dir_path}')
+
+            for directory in dir_names:
+                self.write_into_txt_file(self.generated_txt_file_path, f'a directory: {directory}')
+                self.write_into_excel_file(self.generated_excel_file_path, f'a directory: {directory}')
+
+            for file in file_names:
+                self.write_into_txt_file(self.generated_txt_file_path, f'a file: {file}')
+                self.write_into_excel_file(self.generated_excel_file_path, f'a file: {file}')
+
+            self.write_into_txt_file(self.generated_txt_file_path, '\n')
+            self.write_into_excel_file(self.generated_excel_file_path, '\n')
+
+    def run(self):
+        self.window.mainloop()
 
 
-def empty_the_output_excel_file(file_path):
-    if exists(file_path):
-        os.remove(file_path)
-
-
-def write_into_txt_file(file_path, content):
-    with open(file_path, 'a') as output_file:
-        output_file.write(content + '\n')
-
-
-def write_into_excel_file(file_path, content):
-    workbook = Workbook()                 # creates new workbook
-    worksheet = workbook.active
-    worksheet.title = "Data"
-
-    # worksheet.append(['Daniel', 'is', 'superman'])  # add to the end of the content, first row on diff cols
-    # worksheet.append(['Maxi', 'is', 'superman'])    # next row on different columns
-
-    global current_excel_cell
-    worksheet[current_excel_cell] = content
-
-    number = int(current_excel_cell[1])
-    number += 1
-
-    current_excel_cell = current_excel_cell[0] + str(number)
-
-    workbook.save(file_path)
-
-
-def open_dir():
-    filepath = filedialog.askdirectory()
-    global current_folder
-    current_folder = filepath
-    label2.config(text=f"selected: '{current_folder}'")
-
-
-def fix_the_dir_path_str(dir_path):
-    dir_path = dir_path[0:2] + '\\' + dir_path[3:]
-    return dir_path
-
-
-@time_measurement_decorator
-def the_walk_loop():
-    empty_the_output_txt_file(generated_txt_file_path)
-    empty_the_output_excel_file(generated_txt_file_path)
-
-    folder = current_folder
-
-    for (dir_path, dir_names, file_names) in walk(folder):
-
-        dir_path = fix_the_dir_path_str(dir_path)
-
-        write_into_txt_file(generated_txt_file_path, f'Directory: {dir_path}')
-        for directory in dir_names:
-            write_into_txt_file(generated_txt_file_path, f'a directory: {directory}')
-            write_into_excel_file(generated_excel_file_path, f'a directory: {directory}')
-
-        for file in file_names:
-            write_into_txt_file(generated_txt_file_path, f'a file: {file}')
-            write_into_excel_file(generated_excel_file_path, f'a file: {file}')
-
-        write_into_txt_file(generated_txt_file_path, '\n')
-
-
-# target directory
-# --------------------------------------------------------------------
-current_folder = ''
-
-
-# the output files
-# --------------------------------------------------------------------
-generated_txt_file_path = './generated_txt_file.txt'
-generated_excel_file_path = './generated_excel_file.xlsx'
-# empty_the_output_txt_file(generated_txt_file_path)
-
-current_excel_cell = 'A1'
-
-
-# tkinter gui
-# --------------------------------------------------------------------
-
-window = Tk()
-window.geometry("300x300")
-window.title("dir walker")
-window.config(background='#2b2828')
-
-label1 = Label(window, text='select target dir', width=30, height=1,
-               bg='#2b2828', borderwidth=0, relief="ridge", fg='white')
-label1.grid(row=0, column=0)
-
-button1 = Button(window, text='browse', width=30, height=1, command=open_dir)
-button1.grid(row=1, column=0)
-
-label2 = Label(window, text=current_folder, width=30, height=1,
-               bg='#2b2828', borderwidth=0, relief="ridge", fg='white')
-label2.grid(row=2, column=0)
-
-button2 = Button(window, text='generate', width=30, height=1, command=the_walk_loop)
-button2.grid(row=3, column=0)
-
-label3 = Label(window, text='', width=30, height=1,
-               bg='#2b2828', borderwidth=0, relief="ridge", fg='white')
-label3.grid(row=4, column=0)
-
-
-# gui loop
-# --------------------------------------------------------------------
-window.mainloop()
+new_gui = DirWalkerGui()
+new_gui.run()
